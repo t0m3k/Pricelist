25953ad3e6cca1041e3a06d6c60597a4691a3806
Tomasz Tracz <t@tracz.me>
2017-11-16 22:12:48 +0000

    test authentication for pricelist

--- middleware/index.js
+++ middleware/index.js
@@ -7,6 +7,13 @@
     }
     req.flash("error", "Please login first!");
     res.redirect("/login");
+}; 
+
+middlewareObj.isLoggedInJSON = function(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.send("Not logged in");
 };
 
  module.exports = middlewareObj;
--- routes/pricelist.js
+++ routes/pricelist.js
@@ -6,4 +6,8 @@ router.get("/", middleware.isLoggedIn, function(req, res) {
     res.render("pricelist");
 });
 
+router.get("/get", middleware.isLoggedInJSON, function(req, res) {
+    res.send(req.user);
+});
+
 module.exports = router;
