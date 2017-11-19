c86ba65b37fd5809a80af8c0ee267cd4106d09bc
Tomasz Tracz <t@tracz.me>
2017-11-19 14:10:22 +0000

    Remove loaded second time script user.js

--- public/js/admin/user.js
+++ public/js/admin/user.js
@@ -4,10 +4,10 @@ $('li').click(function(){
     var attr = $(this).attr('id');
 
     // toggle the class for pressed element
-    $(this).children('i').toggleClass("text-danger")
-        .toggleClass("text-success")
-        .toggleClass("fa-square")
-        .toggleClass("fa-check-square");
+    $(this).children('i').toggleClass("text-danger");
+    $(this).children('i').toggleClass("text-success");
+    $(this).children('i').toggleClass("fa-square");
+    $(this).children('i').toggleClass("fa-check-square");
 
     // save elemend id ass attribute for user class
     user[attr] = $(this).children('i').hasClass("text-success");
