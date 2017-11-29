93fafd913c185ad294bfddc060719b8e91053e23
Tomasz Tracz <t@tracz.me>
2017-11-29 12:18:32 +0000

    Add code preparing form to save the change when editing price

--- public/js/pricelist/forms.js
+++ public/js/pricelist/forms.js
@@ -15,40 +15,16 @@ $("#partForm").submit(function (e) {
     parent.$.fancybox.close();
 });
 
-
-// // updates add/edit form with element that will be edited or dataholder if adding new element
-// function editPrice(modelId, priceId) {
-//     clearAddForm(); // clears form and all tmp data
-//     var model = mainData.find(model => {
-//         return model._id == modelId;
-//     });
-//     console.log(model);
-//     var part = model.prices.find(price => {
-//         return price._id == priceId;
-//     });
-//     console.log(part);
-//     $("#localId").val(priceId);
-//     $("#part").val(part.name);
-//     $("#labour").val(part.labour);
-//     $("#minPercentage").val(part.min);
-//     $("#second").val(part.second);
-//     if (part.parts) {
-//         listParts(part.parts);
-        
-//         console.log("Editing item id: " + part._id);
-//     }
+// // updates element in mainData with infro from form
+// function updateFromForm() {
+//     var id = $("#localId").val();
+//     mainData[id].model = $("#model").val();
+//     mainData[id].part = $("#part").val();
+//     mainData[id].labour = $("#labour").val();
+//     mainData[id].min = $("#minPercentage").val();
+//     mainData[id].second = $("#second").val();
+//     mainData[id].parts = parts_tmp;
 // }
-
-// updates element in mainData with infro from form
-function updateFromForm() {
-    var id = $("#localId").val();
-    mainData[id].model = $("#model").val();
-    mainData[id].part = $("#part").val();
-    mainData[id].labour = $("#labour").val();
-    mainData[id].min = $("#minPercentage").val();
-    mainData[id].second = $("#second").val();
-    mainData[id].parts = parts_tmp;
-}
 
 // clears add/edit form
 function clearAddForm() {
@@ -61,58 +37,9 @@ function clearAddForm() {
     $("#partNumber").removeClass("is-invalid");
 }
 
-// add parts button
-$("#addPartButton").click(function () {
-    // runs getPart function that will confirm if part is genuine 
-    getPart($("#partNumber").val(), function (newPart) {
-        var amount = $("#partAmount").val();
-        $("#partNumber").removeClass("is-invalid");
-        
-        if (newPart) { // check if part was returned
-            parts_tmp.push({
-                part:  newPart.part,
-                amount: amount,
-                description: newPart.description,
-                cost: newPart.cost
-            });
-            
-            // add part to parts_tmp, they'll be saved to database when form is successfully submitted
-            listParts(parts_tmp);
 
-            //after part is successfuly added clear add part fields
-            $("#partNumber").val('');
-            $("#partAmount").val('1');
-        } else {
-            $("#partNumber").addClass("is-invalid");
-            console.log("Couldn't find part number, check: " + sql_getpart);
-        }
-    });
-});
 
-function listParts(parts) {
-    var parts_html = '<div><label for="parts">Parts:</label></div>';
-    parts.forEach((part) => {
-        parts_html += '<div class="row mt-3"><div class="col-9"><input type="text" class="form-control parts-text form-control-sm" readonly value="' + part.part.part + '"></div>';
-        parts_html += '<div class="col"><input type="text" class="form-control parts-text form-control-sm" readonly value="' + part.amount + '"></div></div>';
-        parts_html +=   '<div class="row"><div class="col-9">' +
-                        '<input type="text" class="form-control parts-text form-control-sm" readonly value="' + part.part.description + '">' +
-                        '</div>' +
-                        '<div class="col">' +
-                        '<input type="text" class="form-control parts-text form-control-sm" readonly value="' + (part.part.cost * 1.2).toFixed(2) + '">' +
-                        '</div></div>';
-    }, this);
-    
-    parts_html += '<a href="javascript:;" class="btn btn-danger btn-sm top-buffer" onclick="removeParts()">Remove parts</a>';
-    $('#partsDiv').removeClass('hidden').html(parts_html);
-}
 
-function removeParts() {
-    console.log("Removing all parts");
-    $("#partNumber").val('');
-    $("#partAmount").val('1');
-    $('#partsDiv').addClass('hidden');
-    parts_tmp = [];
-}
 
 function editPriceRow(row) {
     var data = JSON.stringify(mainData[row]);
--- public/js/pricelist/write.js
+++ public/js/pricelist/write.js
@@ -71,6 +71,24 @@ function editPrice(modelId, priceId) {
     })
 }
 
+
+function listParts(parts) {
+    var parts_html = '<div><label for="parts">Parts:</label></div>';
+    parts.forEach((part) => {
+        parts_html += '<div class="row mt-3"><div class="col-9"><input type="text" class="form-control parts-text form-control-sm" readonly value="' + part.part.part + '"></div>';
+        parts_html += '<div class="col"><input type="text" class="form-control parts-text form-control-sm" readonly value="' + part.amount + '"></div></div>';
+        parts_html +=   '<div class="row"><div class="col-9">' +
+                            '<input type="text" class="form-control parts-text form-control-sm" readonly value="' + part.part.description + '">' +
+                        '</div>' +
+                        '<div class="col">' +
+                            '<input type="text" class="form-control parts-text form-control-sm" readonly value="' + (part.part.cost * 1.2).toFixed(2) + '">' +
+                        '</div></div>';
+    });
+    
+    parts_html += '<a href="javascript:;" class="btn btn-danger btn-sm top-buffer" onclick="removeParts()">Remove parts</a>';
+    $('#partsDiv').removeClass('hidden').html(parts_html);
+}
+
 function addDeletePrice() {
     // add delete icon to every price item
     $(".price-item").each(function(){
@@ -106,3 +124,39 @@ function removePrice(model, price) {
             showPopUp("Success!", "Your changes have been saved.", 1500);
     });
 }
+
+function removeParts() {
+    console.log("Removing all parts");
+    $("#partNumber").val('');
+    $("#partAmount").val('1');
+    $('#partsDiv').addClass('hidden');
+    
+}
+
+// add parts button
+$("#addPartButton").click(function () {
+    // runs getPart function that will confirm if part is genuine 
+    getPart($("#partNumber").val(), function (newPart) {
+        var amount = $("#partAmount").val();
+        $("#partNumber").removeClass("is-invalid");
+        
+        if (newPart) { // check if part was returned
+            parts_tmp.push({
+                part:  newPart.part,
+                amount: amount,
+                description: newPart.description,
+                cost: newPart.cost
+            });
+            
+            // add part to parts_tmp, they'll be saved to database when form is successfully submitted
+            listParts(parts_tmp);
+
+            //after part is successfuly added clear add part fields
+            $("#partNumber").val('');
+            $("#partAmount").val('1');
+        } else {
+            $("#partNumber").addClass("is-invalid");
+            console.log("Couldn't find part number, check: " + sql_getpart);
+        }
+    });
+});
